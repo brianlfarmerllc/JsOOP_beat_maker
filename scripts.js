c3eda3b56dcdb6cbcfee84f2eea8571f3e3a43ec
brianlfarmerllc <brianlfarmerllc@gmail.com>
2021-06-28 02:35:52 -0500

    add java script to index through pads playing active sounds

---
+++ scripts.js
@@ -0,0 +1,56 @@
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.playButton = document.querySelector(".play");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
+    this.bpm = 150;
+    this.index = 0;
+  }
+  activePad() {
+    this.classList.toggle("active");
+  }
+  repeat() {
+    let step = this.index % 8;
+    this.index++;
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      if (bar.classList.contains("active")) {
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
+  }
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+    setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
+}
+
+const drumKit = new DrumKit();
+
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
+});
+
+// adding callback to invoke method in event listener to maintain this keyword reference
+drumKit.playButton.addEventListener("click", () => {
+  drumKit.start();
+});
